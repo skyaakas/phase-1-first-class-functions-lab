9e896c1e286a301b8656535e1ede9e5d48e44a8d
+ <+>
2022-06-28 10:34:08 -0600

    commit tree 009120798d383bb8992fd995cc85b1295db6f3d2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(scuberArray){
+    return scuberArray.slice(0,2);
+}
+function returnLastTwoDrivers(scuberArray){
+    return scuberArray.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(a){
+    return function fareQuintupler(fare){
+        return fare * a;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, scuberArray){
+    return drivers.scuberArray;
+}
+    
+
